f0f32b201940185fba31421b06d9c4b81d6d2f2d
Artur Wronski <a.wronski@mgmoptima.com>
2017-06-19 16:26:35 +0200

    Forking testing

--- src/awesome.js
+++ src/awesome.js
@@ -1,6 +1,6 @@
 export default {
   sauce: function(){
-    alert('your crap website is now been awesome sauced');
-    console.log('your crap website website is now awesome');
+    alert('your excellent website is now been awesome sauced');
+    console.log('your excellent website website is now awesome');
   }
 }
