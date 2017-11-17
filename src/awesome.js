9147966e6471f4c35d6e720158926a5ed2a592bc
arthur_d <arthurd800@wp.pl>
2017-11-17 16:05:46 +0100

    added console.log

--- src/awesome.js
+++ src/awesome.js
@@ -1,6 +1,7 @@
 export default {
   sauce: function(){
     alert('your crap website is now been awesome sauced');
+    console.log("#####");
     console.log('YOUR crap website is now awesome!');
   }
 }
