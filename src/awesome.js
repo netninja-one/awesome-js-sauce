f36edfbb87d325fafb933be12d27c780d00efa33
niladri <nmahato@aapnainfotech.in>
2018-08-16 18:23:23 +0530

    Changed DOM

--- src/awesome.js
+++ src/awesome.js
@@ -2,5 +2,6 @@ export default {
   sauce: function(){
     alert('your crap website is now been awesome sauced');
     console.log('your crap website website is now awesome');
+	document.write("Thanks for the tutorials! Keep up the good work! Helps a lot!");
   }
 }
