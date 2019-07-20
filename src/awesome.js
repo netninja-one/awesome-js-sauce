7a9fa7114fad5d15100aa605a5883ac4b42e6a87
Gabszy10 <corderogabrielle@gmail.com>
2019-07-20 22:26:43 +0800

    Improved awesome js

--- src/awesome.js
+++ src/awesome.js
@@ -2,5 +2,6 @@ export default {
   sauce: function(){
     alert('your crap website is now been awesome sauced');
     console.log('your crap website website is now awesome');
+    console.log('GabszyztyLe is Pogi! SHOUT OUT TO FILIPINOS OUT THERE! ADD ME ON FB GABRIELLE CORDERO!')
   }
 }
