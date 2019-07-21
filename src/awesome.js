153ebb9ff6d40193868fe8c25cb36416777b514d
Gabszy10 <corderogabrielle@gmail.com>
2019-07-21 10:13:53 +0800

    Added some files in awesome js

--- src/awesome.js
+++ src/awesome.js
@@ -3,5 +3,6 @@ export default {
     alert('your crap website is now been awesome sauced');
     console.log('your crap website website is now awesome');
     console.log('GabszyztyLe is Pogi! SHOUT OUT TO FILIPINOS OUT THERE! ADD ME ON FB GABRIELLE CORDERO!')
+    console.log('GabszyztyLe is Pogi! SHOUT OUT TO FILIPINOS OUT THERE! ADD ME ON FB GABRIELLE CORDERO!')
   }
 }
