d782af913f1dda5b20c272dc6ca2c7b9cfdcb7c0
bc2mighty <akinadetunmise8@gmail.com>
2020-04-10 21:57:16 +0100

    Improved awesome file

--- src/awesome.js
+++ src/awesome.js
@@ -1,6 +1,8 @@
 export default {
   sauce: function(){
     alert('your crap website is now been awesome sauced');
-    console.log('your crap website website is now awesome');
+    // console.log('your crap website website is now awesome');
+    console.log("It's my time homie. let me make my comment bro!");
+    
   }
 }
