ba9143d9ac600f8aa452f60ca80ee1e69132f254
Rudrajeet Pal <rudrajeetpal64@gmail.com>
2020-04-17 16:12:16 +0530

    Use Arrow Functions

--- src/awesome.js
+++ src/awesome.js
@@ -1,5 +1,5 @@
 export default {
-  sauce: function(){
+  sauce: () => {
     alert('your crap website is now been awesome sauced');
     console.log('your crap website website is now awesome');
   }
