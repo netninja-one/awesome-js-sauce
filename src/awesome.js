42343026426dbac5db3b5a7aa20332c4157f1831
Jabba1661 <jabba1616@gmail.com>
2020-04-25 07:14:54 +0400

    improved awesome.js, added sauce2() function

--- src/awesome.js
+++ src/awesome.js
@@ -1,6 +1,10 @@
 export default {
-  sauce: function(){
-    alert('your crap website is now been awesome sauced');
-    console.log('your crap website website is now awesome');
-  }
-}
+  sauce: function () {
+    alert("your crap website is now been awesome sauced");
+    console.log("your crap website website is now awesome");
+  },
+  sauce2: function () {
+    alert("added sauce2");
+    console.log("console log..added sauce2()");
+  },
+};
