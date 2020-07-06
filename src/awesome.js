7bd12fe6ec9aba2e2968afd0881d05acbb2e2085
abdoabdoabdoabdoabdo <abdonaggar77@gmail.com>
2020-07-06 17:19:05 +0200

    added r to console.log

--- src/awesome.js
+++ src/awesome.js
@@ -1,6 +1,6 @@
 export default {
   sauce: function(){
     alert('your crap website is now been awesome sauced');
-    console.log('your crap website website is now awesome');
+    console.log('your crap website website is now awesomer');
   }
 }
