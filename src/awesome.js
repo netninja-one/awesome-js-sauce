0f6be4070bdaab0b685950ea502e5aa0d96d9103
ivarshilov <ilko.k.v@gmail.com>
2020-07-17 17:25:16 +0300

    improve awesome.js 17 jul 2020

--- src/awesome.js
+++ src/awesome.js
@@ -2,5 +2,7 @@ export default {
   sauce: function(){
     alert('your crap website is now been awesome sauced');
     console.log('your crap website website is now awesome');
+	
+	console.log('my test 17 Jul 2020');
   }
 }
