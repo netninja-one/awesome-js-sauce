7749d1da0c00f5121463298f4e130ad3c2c161cb
SirDave001 <82815174+SirDave001@users.noreply.github.com>
2021-05-16 17:59:12 -0700

    improved

--- src/awesome.js
+++ src/awesome.js
@@ -2,5 +2,6 @@ export default {
   sauce: function(){
     alert('your crap website is now been awesome sauced');
     console.log('your crap website website is now awesome');
+    console.log('yo! i am about to finish this and move on');
   }
 }
