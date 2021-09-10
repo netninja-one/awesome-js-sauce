db4c05052c83066025ccf1bd9f28ca146bb7a063
Danish habib <spober_woh@yahoo.com>
2021-09-10 08:49:28 -0700

    New Line added

--- src/awesome.js
+++ src/awesome.js
@@ -2,5 +2,6 @@ export default {
   sauce: function(){
     alert('your crap website is now been awesome sauced');
     console.log('your crap website website is now awesome');
+    console.log('is this the good time to learn these basic things!where were you');
   }
 }
