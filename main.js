6dd4b4eeece7049ad359c2892f0f9dcf612d5749
iamsk <saravanankmk17@gmail.com>
2022-03-05 19:00:02 +0530

    thanked shaun

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 // init the awesomeness
 import Awesome from './src/awesome.js'
 Awesome.sauce();
+
+//hello shaun. This is sk and i learnt github from your tutorial very well
+//Thank you so much for that :)
