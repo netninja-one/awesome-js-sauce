6999054f06742d64e5d7dad1edd8213658bbced3
Manish_Syal <manishsyal77@gmail.com>
2022-09-27 00:09:42 +0530

    added a new alert in awesome.js file

--- src/awesome.js
+++ src/awesome.js
@@ -1,6 +1,7 @@
 export default {
   sauce: function(){
     alert('your crap website is now been awesome sauced');
-    console.log('your crap website website is now awesome');
+    console.log('your crap and all other websites is now awesome');
+    alert("Hello User....! Dont forget to follow this repo");
   }
 }
