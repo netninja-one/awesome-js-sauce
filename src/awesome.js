3cfab75b85b8dd3f28fca32b9f219700d113c1e2
Manish_Syal <manishsyal77@gmail.com>
2022-10-01 21:58:40 +0530

    deleted previous changes

--- src/awesome.js
+++ src/awesome.js
@@ -2,6 +2,5 @@ export default {
   sauce: function(){
     alert('your crap website is now been awesome sauced');
     console.log('your crap and all other websites is now awesome');
-    alert("Hello User....! Dont forget to follow this repo");
   }
 }
