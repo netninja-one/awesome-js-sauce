105c5500d6620becb8591c2fdbf4f68930ce6ede
Omoobaba <chidiebubeiroezindu@gmail.com>
2023-02-01 12:28:07 +0100

    changed a typo

--- src/awesome.js
+++ src/awesome.js
@@ -1,6 +1,6 @@
 export default {
   sauce: function(){
     alert('your crap website is now been awesome sauced');
-    console.log('your bad website website is now awesome');
+    console.log('your bad website is now awesome');
   }
 }
