3c7513b83d635bf4eb65247aa3a0fdda9f49098f
sammy-jeff <okoyechibuike048@gmail.com>
2023-03-23 11:29:24 +0100

    added a cook method

--- src/awesome.js
+++ src/awesome.js
@@ -2,5 +2,8 @@ export default {
   sauce: function(){
     alert('your crap website is now been awesome sauced');
     console.log('your crap website website is now awesome');
+  },
+  cook(){
+    alert("we should keep cooking")
   }
 }
