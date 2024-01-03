a98ed1a444043d532ef4c50819cfc0d70c821200
Tsegaye <tsegshtsegaye30@gmail.com>
2024-01-03 22:36:00 +0300

    add awsome page

--- src/awesome.js
+++ src/awesome.js
@@ -1,6 +1,6 @@
 export default {
   sauce: function(){
     alert('your crap website is now been awesome sauced');
-    console.log('my crap  website is now awesome and pretty for eye wow fuck');
+    console.log('my crap  website is now awesome and pretty for eye');
   }
 }
